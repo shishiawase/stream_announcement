260e4a6250b67b2e92df6b1d90cbc5cb14734fcf
shishiawase <53081951+shishiawase@users.noreply.github.com>
2022-09-18 05:29:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 settings = {
-    'client_id': '',
-    'client_secret': '',
-    'channel_name': '',
-    'discord_token': '',
-    'discord_channel': ''
+    'client_id': '', // your_twitch_application_id
+    'client_secret': '', // your_twitch_secret_key
+    'channel_name': '', // your_twitch_channel_name
+    'discord_token': '', // your_discord_bot
+    'discord_channel': '' // your_discord_server
 }
